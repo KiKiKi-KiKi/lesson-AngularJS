c268b25625434f6e69844cfa8e4790bbbad5d72b
Chaika <chaika.design@gmail.com>
2015-10-20 11:31:48 +0900

    データを５件まで表示するフィルター

--- test/myapp.js
+++ test/myapp.js
@@ -3,7 +3,14 @@ angular.module('app', [])
   $scope.users = [
     {name: 'taguchi', score: 52.22},
     {name: 'tanaka', score: 38.55},
-    {name: 'yamada', score: 68.11}
+    {name: 'yamada', score: 68.11},
+    {name: 'honma', score: 100.00},
+    {name: 'ichikawa', score: 99.98},
+    {name: 'kuritani', score: 50.00},
+    {name: 'hashimoto', score: 42.67},
+    {name: 'shimobayashi', score: 27.88},
+    {name: 'iwata', score: 50.00},
+    {name: 'kobayashi', score: 68.12}
   ];
   $scope.today = new Date();
 });
