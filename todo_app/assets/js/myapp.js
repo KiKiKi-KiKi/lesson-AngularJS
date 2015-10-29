d536247ed5bc9a5e79c10725de2b6936e026a13a
CHAIKA <chaika.design@gmail.com>
2015-10-29 23:37:11 +0900

    #01 display TODO sample

--- todo_app/assets/js/myapp.js
+++ todo_app/assets/js/myapp.js
@@ -1,1 +1,10 @@
-console.log('hoge')
+angular.module('app', [])
+.controller('MainCtrl', function($scope){
+  $scope.tasks = [
+    {id: 1, body:'do this 1', done:false},
+    {id: 2, body:'do this 2', done:false},
+    {id: 3, body:'do this 3', done:true},
+    {id: 4, body:'do this 4', done:false},
+    {id: 5, body:'do this 5', done:false},
+  ];
+});
