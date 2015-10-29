d33f9246e4328d0a5523a766e3644a4dd7dab6f1
CHAIKA <chaika.design@gmail.com>
2015-10-29 23:09:36 +0900

    express use stylus

--- todo_app/app.js
+++ todo_app/app.js
@@ -1,18 +1,36 @@
 var express = require('express'),
     path    = require('path'),
     logger  = require('morgan'),
+    bodyParser = require('body-parser'),
+    stylus  = require('stylus'),
+    nib     = require('nib'),
     app     = express(),
     router  = require('./routes/app');
+
+// log
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // use jade Template
 app.set('views', path.join(__dirname + '/views'));
 app.set('view engine', 'jade');
 
+// use stylus
+app.use(stylus.middleware({
+  src: __dirname + '/views',
+  dest: __dirname + '/assets',
+  compile: function compile(src, path) {
+    return stylus(src)
+     .set('filename', path)
+     .set('compress', true)
+     .use(nib())
+     .import('nib');
+  }
+}));
+
 // set static file dir
 app.use('/assets', express.static(__dirname + '/assets'));
-
-// log
-app.use(logger('dev'));
 
 // Routing
 app.get('/', router.index);
